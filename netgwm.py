b5d12bafab9b5dda8cbf9cf909769e62ff972dcb
Dmitry Shurupov <dmitry.shurupov@flant.com>
2017-08-08 10:41:11 +0700

    Formal switching to GPLv3 and tiny docs fixes

--- netgwm.py
+++ netgwm.py
@@ -4,14 +4,15 @@
 #       NetGWM (Network Gateway Manager) is a tool for
 #       automatically switching gateways when Internet
 #       goes offline.
-#       Home page: https://flant.ru/projects/netgwm
+#
+#       Home page (Russian): https://flant.ru/projects/netgwm
 #
 #       Copyright (C) 2012-2017 CJSC Flant (www.flant.ru)
 #       Written by Andrey Polovov <andrey.polovov@flant.ru>
 #
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
+#       the Free Software Foundation; either version 3 of the License, or
 #       any later version.
 #
 #       This program is distributed in the hope that it will be useful,
@@ -20,9 +21,7 @@
 #       GNU General Public License for more details.
 #
 #       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+#       along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys, os, stat
 import yaml
